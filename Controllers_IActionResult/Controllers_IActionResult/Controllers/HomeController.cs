37d37e7418ba811debabeacfb123f3029347f5dd
José Elias <joseelias.dev@gmail.com>
2023-08-25 22:00:08 -0300

    Estudo sobre Controller e IActionResult

---
+++ Controllers_IActionResult/Controllers_IActionResult/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Controllers_IActionResult.Models;
+
+namespace Controllers_IActionResult.Controllers
+{
+    public class HomeController : Controller
+    {
+        Person p = new Person
+        {
+            Number = 1001,
+            Name = "Jose",
+            Balance = 500
+        };
+
+        [Route ("/")]
+        public IActionResult Index()
+        {
+            return Content("Welcome to my Bank");
+        }
+
+        [Route ("account-details")]
+
+        public IActionResult Details()
+        {
+            return Json(p);
+        }
+
+        [Route("account-statment")]
+        public IActionResult Statment()
+        {
+            return File("/José-Elias-Tarraf-neto.pdf",
+                "application/pdf");
+        }
+
+        [Route("get-balance/{accountNumber:int?}")]
+        public IActionResult Balance()
+        {
+            if (Request.RouteValues.ContainsKey("accountNumber"))
+            {
+                int accN = Convert.ToInt32(Request.RouteValues["accountNumber"]);
+
+                if (accN > 1001)
+                {
+                    return BadRequest("Account Number Less than 1001");
+                }
+
+                if (accN != p.Number)
+                {
+                    return BadRequest("Account Number should be 1001");
+                }
+
+                return Content(p.Balance.ToString());
+            }
+
+            return NotFound("Account Number not Supplied");
+        }
+    }   
+}
---
+++ Controllers_IActionResult/Controllers_IActionResult/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Controllers_IActionResult.Models
+{
+    public class Person
+    {
+        public int Number { get; set; }
+        public string? Name { get; set; }
+        public int Balance { get; set; }
+    }
+}
